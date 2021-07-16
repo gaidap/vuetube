2aac151d618bbcdfc6087291e31b15249b78da6c
gaidap <pc.gaida@gmail.com>
2021-07-16 21:23:55 +0200

    Refactoring to computed functions and more styling

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,13 +1,38 @@
 <template>
-  <li class="list-group-item">
-    <img class="thumbnail" :src="video.snippet.thumbnails.default.url" :alt="video.id.videoId"/>
-    {{ video.snippet.title }}
+  <li class="list-group-item media">
+    <img class="mr-3" :src="thumbnailUrl" :alt="thumbnailAlternative"/>
+    <div class="media-body">
+      {{ video.snippet.title }}
+    </div>
   </li>
 </template>
 
 <script>
 export default {
   name: "VideoListItem",
-  props: ['video']
+  props: ['video'],
+  computed: {
+    thumbnailUrl() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+    thumbnailAlternative() {
+      return this.video.id.videoId;
+    }
+  }
 }
 </script>
+
+<style scoped>
+  li {
+    display: flex;
+    cursor: pointer;
+  }
+
+  li:hover {
+    background-color: lightgray;
+  }
+
+  div {
+    margin: 5px;
+  }
+</style>
